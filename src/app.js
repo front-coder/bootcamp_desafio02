b2ac9a8ae7b6118da9ec275830067d83f99dc213
front-coder <alex.mac.general@gmail.com>
2020-02-12 00:37:46 -0300

    Ref.: #1
    Configurados prettier, editorconfig e eslint

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-    constructor(){
-        this.server = express();
+	constructor() {
+		this.server = express();
 
-        this.midlewares();
-        this.routes();
-    }
+		this.midlewares();
+		this.routes();
+	}
 
-    midlewares() {
-        this.server.use(express.json());
-    }
+	midlewares() {
+		this.server.use(express.json());
+	}
 
-    routes() {
-        this.server.use(routes);
-    }
+	routes() {
+		this.server.use(routes);
+	}
 }
 
 export default new App().server;
